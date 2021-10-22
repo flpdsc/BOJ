b442d42b471b77b802ecc460a111837410ea2900
flpdsc <92yjkim@gmail.com>
2021-10-22 16:21:17 +0900

    1022 수 정렬하기

---
+++ cpp/004.cpp
@@ -0,0 +1,15 @@
+//수 정렬하기 https://www.acmicpc.net/problem/2750
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; ++i) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    for(int i=0; i<n; ++i) cout << arr[i] << endl;
+    return 0;
+}
