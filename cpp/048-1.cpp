e591ad44e0a83f38463d21e1785b8a7bf3629503
flpdsc <92yjkim@gmail.com>
2021-12-04 23:07:40 +0900

    1204 정수 삼각형

---
+++ cpp/048-1.cpp
@@ -0,0 +1,30 @@
+//정수 삼각형 https://www.acmicpc.net/problem/1932 - Bottom-Up
+#include <iostream>
+using namespace std;
+
+int tree[501][501], dp[501][501];
+
+int main()
+{
+    int n, res=-2147000000;
+    cin >> n;
+    for(int i=1; i<=n; ++i){
+        for(int j=1; j<=i; ++j){
+            cin >> tree[i][j];
+        }
+    }
+
+    dp[1][1] = tree[1][1];
+
+    for(int i=2; i<=n; ++i){
+        for(int j=1; j<=i; ++j){
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j])+tree[i][j];
+        }
+    }
+
+    for(int i=1; i<=n; ++i) res = max(res, dp[n][i]);
+
+    cout << res << '\n';
+
+    return 0;
+}
---
+++ cpp/048-2.cpp
@@ -0,0 +1,24 @@
+//정수 삼각형 https://www.acmicpc.net/problem/1932 - Top-Down
+#include <iostream>
+using namespace std;
+
+int n, tree[501][501], dp[501][501];
+
+int findMax(int l, int p)
+{
+    if(dp[l][p]>0) return dp[l][p]; 
+    if(l==n) return tree[l][p];
+    else return max(findMax(l+1, p), findMax(l+1, p+1))+tree[l][p];
+}
+
+int main()
+{
+    cin >> n;
+    for(int i=1; i<=n; ++i){
+        for(int j=1; j<=i; ++j){
+            cin >> tree[i][j];
+        }
+    }
+    cout << findMax(1, 1) << '\n';
+    return 0;
+}
