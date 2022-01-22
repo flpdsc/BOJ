6ebcee42f3bc0cab82276225b661e1fcf9917ab8
flpdsc <92yjkim@gmail.com>
2022-01-22 23:57:30 +0900

    0122 점프왕 쩰리

--- README.md
+++ README.md
@@ -95,6 +95,7 @@
 094. [뱀과 사다리 게임](https://www.acmicpc.net/problem/16928) 220119
 095. [경쟁적 전염](https://www.acmicpc.net/problem/18405) 220120
 096. [데스 나이트](https://www.acmicpc.net/problem/16948) 220121
+097. [점프왕 쩰리(Small)](https://www.acmicpc.net/problem/16173) 220122
 ### 문제집
 * [단기간 성장](https://www.acmicpc.net/workbook/view/4349)
 * [BOJ 길라잡이 베타 (1)](https://www.acmicpc.net/workbook/view/2418)
---
+++ cpp/097.cpp
@@ -0,0 +1,41 @@
+//점프왕 쩰리 https://www.acmicpc.net/problem/16173
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int map[3][3];
+bool visited[3][3];
+int dx[2] = {1, 0}, dy[2]={0, 1};
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=0; i<n; ++i)
+        for(int j=0; j<n; ++j)
+            cin >> map[i][j];
+
+    queue<pair<int, int> > q;
+    q.push(make_pair(0, 0));
+    visited[0][0] = true;
+    while(!q.empty())
+    {
+        pair<int, int> cur = q.front();
+        int p = map[cur.first][cur.second];
+        q.pop();
+        if(cur.first==n-1 && cur.second==n-1) break;
+        else{
+            for(int i=0; i<2; ++i){
+                int xx = cur.second+dx[i]*p;
+                int yy = cur.first+dy[i]*p;
+                if(xx>=0 && yy>=0 && xx<n && yy<n && !visited[yy][xx]){
+                    q.push(make_pair(xx, yy));
+                    visited[yy][xx] = true;
+                }
+            }
+        }
+    }
+    if(visited[n-1][n-1]) cout << "HaruHaru\n";
+    else cout << "Hing\n";
+    return 0;
+}
