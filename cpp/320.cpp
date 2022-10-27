5836ec1c1604961f7898ecb89146d9978f7c80cd
flpdsc <92yjkim@gmail.com>
2022-10-27 15:51:31 +0900

    1025 곱셈

---
+++ cpp/320.cpp
@@ -0,0 +1,14 @@
+//곱셈 https://www.acmicpc.net/problem/2588
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << a * (b%10) << '\n';
+    cout << a *((b%100)/10) << '\n';    
+    cout << a * (b/100) << '\n';
+    cout << a * b << '\n';
+    return 0;
+}
