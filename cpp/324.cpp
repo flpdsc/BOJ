cb73a63bb505049e071caafeff5dd239feb2546f
flpdsc <92yjkim@gmail.com>
2022-11-04 21:46:51 +0900

    1104 문자열 반복

---
+++ cpp/324.cpp
@@ -0,0 +1,24 @@
+//문자열 반복 https://www.acmicpc.net/problem/2675
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int r;
+        string s;
+        cin >> r >> s;
+        for(int i=0; i<s.size(); ++i)
+        {
+            for(int j=0; j<r; ++j)
+            {
+                cout << s[i];
+            }
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
